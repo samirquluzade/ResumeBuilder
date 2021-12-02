33174545cef1851f41444932a0c6db4cb55f892f
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-12-02 19:00:09 +0400

    Created: (Modal for signup and signin)

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from "./Header";
 import MainSection from "./MainSection";
 import Footer from "./Footer";
+import {Button, Modal} from "react-bootstrap";
 
 const Home = (props) => {
     return(
