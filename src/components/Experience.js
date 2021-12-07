fbd28ef29bcd5b6bee1ff4558e73588c461d6c82
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-12-07 18:18:41 +0400

    Updated:(Experience section v1.2)

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,28 +2,44 @@ import React from 'react';
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
-const Experience = ({handleChange,backTo}) => {
+const Experience = ({handleChange,backTo,data}) => {
+
+    const checkHandler = () => {
+        let checkbox = document.getElementById('present');
+        let text = document.getElementById('end');
+        if(checkbox.checked === true){
+            text.style.display = "none";
+            data.endDate = "Present";
+        }
+        else
+        {
+            text.style.display = "block";
+        }
+    }
+
     return(
         <>
             <Title>Tell us about your Experience</Title>
             <Message>Start with your recent job</Message>
             <Inputs>
                 <Label htmlFor="job">Job Title</Label>
-                <Input type="text" name="job" className="form-control" placeholder="Software Engineer" onChange={handleChange}/>
+                <Input type="text" name="job" className="form-control" placeholder="Software Engineer" onChange={handleChange} value={data.job}/>
                 <Label htmlFor="employer">Employer</Label>
-                <Input type="text" name="employer" className="form-control" placeholder="Google" onChange={handleChange}/>
+                <Input type="text" name="employer" className="form-control" placeholder="Google" onChange={handleChange} value={data.employer}/>
                 <Label htmlFor="startDate">Start Date</Label>
-                <Input type="month" name="startDate" className="form-control" onChange={handleChange}/>
-                <Label htmlFor="endDate">End Date</Label>
-                <Input type="month" name="endDate" className="form-control" onChange={handleChange}/>
-                <Label style={{cursor:'pointer'}}>
-                    <Input type="checkbox" style={{marginTop:'4%',float:'left'}}/> &nbsp; I currently work here
+                <Input type="month" name="startDate" className="form-control" onChange={handleChange} value={data.startDate}/>
+                <EndDate>
+                    <Label htmlFor="endDate">End Date</Label>
+                    <Input type="month" name="endDate" className="form-control" onChange={handleChange} value={data.endDate} id="end"/>
+                </EndDate>
+                <Label style={{cursor:'pointer'}} onClick={checkHandler}>
+                    <Input type="checkbox" style={{marginTop:'4%',float:'left'}} id="present" /> &nbsp; I currently work here
                 </Label>
                 <br/>
                 <Label htmlFor="city">City</Label>
-                <Input type="text" name="city" className="form-control" onChange={handleChange}/>
+                <Input type="text" name="city" className="form-control" onChange={handleChange} value={data.city}/>
                 <Label htmlFor="desc">Description</Label>
-                <TextArea name="desc" className="form-control" rows="5" cols="50" maxLength="250" placeholder="Write your work experience" onChange={handleChange}/>
+                <TextArea name="desc" className="form-control" rows="5" cols="50" maxLength="250" placeholder="Write your work experience" onChange={handleChange} value={data.desc}/>
                 <Next>
                     <Link to="#contact" className="btn btn-danger" onClick={backTo}>Back</Link>
                     <Link to="#experience" className="btn btn-primary">Next to Education</Link>
@@ -70,5 +86,7 @@ const Input = styled.input`
 const TextArea = styled.textarea`
     resize: none;
 `;
+
+const EndDate = styled.div``;
 
 export default Experience;
