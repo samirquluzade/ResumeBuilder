53ff75d4def43b952dcdfb72a7c38226a4705f0a
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-12-08 23:43:47 +0400

    Created:(Inputs in details tag)

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -37,40 +37,53 @@ const Experience = ({handleChange,backTo,data,goToEducation,errors}) => {
         }
     }
 
+    const addExperience = () => {
+        let details = document.createElement("details");
+        // let summary = document.createElement("summary");
+        let inputs = document.getElementById("inputs");
+
+        details.appendChild(inputs);
+        document.getElementById('details').appendChild(details);
+        // document.getElementById('details').insertAdjacentElement('afterend',inputs);
+    }
+
     return(
         <>
             <Title>Tell us about your Experience</Title>
             <Message>Start with your recent job</Message>
-            <Inputs>
-                <Label htmlFor="job">Job Title</Label>
-                <Input type="text" name="job" className="form-control" placeholder="Software Engineer" onChange={handleChange} value={data.job} minLength={2} maxLength={20} required="required"/>
-                {errors.job && <Error>{errors.job}</Error>}
-                <Label htmlFor="employer">Employer</Label>
-                <Input type="text" name="employer" className="form-control" placeholder="Google" onChange={handleChange} value={data.employer} minLength={2} maxLength={20} required="required"/>
-                {errors.employer && <Error>{errors.employer}</Error>}
-                <Label htmlFor="startDate">Start Date</Label>
-                <Input type="month" name="startDate" className="form-control" onChange={handleChange} value={data.startDate}/>
-                {errors.startDate && <Error>{errors.startDate}</Error>}
-                <EndDate>
-                    <Label htmlFor="endDate">End Date</Label>
-                    <Input type="month" name="endDate" className="form-control" onChange={checkDate} value={data.endDate} id="end"/>
-                    {error.endDate && <Error>{error.endDate}</Error>}
-                    {errors.endDate && <Error>{errors.endDate}</Error>}
-                </EndDate>
-                <Label style={{cursor:'pointer'}} onClick={checkHandler}>
-                    <Input type="checkbox" style={{marginTop:'4%',float:'left'}} id="present" /> &nbsp; I currently work here
-                </Label>
-                <br/>
-                <Label htmlFor="city">City</Label>
-                <Input type="text" name="city" className="form-control" onChange={handleChange} value={data.city} minLength={2} maxLength={20} required="required"/>
-                {errors.city && <Error>{errors.city}</Error>}
-                <Label htmlFor="desc">Description</Label>
-                <TextArea name="desc" className="form-control" rows="5" cols="50" maxLength="250" placeholder="Write your work experience" onChange={handleChange} value={data.desc}/>
-                <Next>
-                    <Link to="#contact" className="btn btn-danger" onClick={backTo}>Back</Link>
-                    <Link to="#education" className="btn btn-primary" onClick={goToEducation}>Next to Education</Link>
-                </Next>
-            </Inputs>
+            <Details id="details">
+                <Inputs id="inputs">
+                    <Label htmlFor="job">Job Title</Label>
+                    <Input type="text" name="job" className="form-control" placeholder="Software Engineer" onChange={handleChange} value={data.job} minLength={2} maxLength={20} required="required"/>
+                    {errors.job && <Error>{errors.job}</Error>}
+                    <Label htmlFor="employer">Employer</Label>
+                    <Input type="text" name="employer" className="form-control" placeholder="Google" onChange={handleChange} value={data.employer} minLength={2} maxLength={20} required="required"/>
+                    {errors.employer && <Error>{errors.employer}</Error>}
+                    <Label htmlFor="startDate">Start Date</Label>
+                    <Input type="month" name="startDate" className="form-control" onChange={handleChange} value={data.startDate}/>
+                    {errors.startDate && <Error>{errors.startDate}</Error>}
+                    <EndDate>
+                        <Label htmlFor="endDate">End Date</Label>
+                        <Input type="month" name="endDate" className="form-control" onChange={checkDate} value={data.endDate} id="end"/>
+                        {error.endDate && <Error>{error.endDate}</Error>}
+                        {errors.endDate && <Error>{errors.endDate}</Error>}
+                    </EndDate>
+                    <Label style={{cursor:'pointer'}} onClick={checkHandler}>
+                        <Input type="checkbox" style={{marginTop:'4%',float:'left'}} id="present" /> &nbsp; I currently work here
+                    </Label>
+                    <br/>
+                    <Label htmlFor="city">City</Label>
+                    <Input type="text" name="city" className="form-control" onChange={handleChange} value={data.city} minLength={2} maxLength={20} required="required"/>
+                    {errors.city && <Error>{errors.city}</Error>}
+                    <Label htmlFor="desc">Description</Label>
+                    <TextArea name="desc" className="form-control" rows="5" cols="50" maxLength="250" placeholder="Write your work experience" onChange={handleChange} value={data.desc}/>
+                </Inputs>
+            </Details>
+            <Next>
+                <Link to="#contact" className="btn btn-danger" onClick={backTo}>Back</Link>
+                <Link to="#experience" className="btn btn-info" onClick={addExperience}>Add new Experience</Link>
+                <Link to="#education" className="btn btn-primary" onClick={goToEducation}>Next to Education</Link>
+            </Next>
         </>
     );
 }
@@ -117,6 +130,10 @@ const EndDate = styled.div``;
 
 const Error = styled.p`
   color:red;
+`;
+
+const Details = styled.div`
+
 `;
 
 export default Experience;
